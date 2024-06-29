6f42888f04df9afac8516f45cbc2330f700c4b38
dinhthienan33 <134404922+dinhthienan33@users.noreply.github.com>
2024-06-30 06:40:54 +0700

    layout

--- app2.py
+++ app2.py
@@ -92,11 +92,11 @@ def analyze_image_information(image_description, ocr_results,objects_names):
 # Streamlit app
 st.set_page_config(layout="wide")
 st.title("Image Analysis App")
-# logo_path = "img\logo.png"  # Change this to the path of your logo file or a URL
-# logo = Image.open(logo_path)
-# st.image(logo, width=100)
-# st.text("The Team")     
-# st.markdown("Your Name") 
+logo_path = "img\logo.png"  # Change this to the path of your logo file or a URL
+logo = Image.open(logo_path)
+st.image(logo, width=100)
+st.text("The Team")     
+st.markdown("Hack HCM2024") 
 # Create three columns with custom widths
 col1, col2, col3 = st.columns([1, 2, 2])
 
