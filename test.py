42299a2fe1925c4566b700fc5b6fdc265a7e815e
dinhthienan33 <134404922+dinhthienan33@users.noreply.github.com>
2024-06-30 11:19:06 +0700

    last

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
-import easyocr
-def perform_ocr(image):
-    ocr_reader = easyocr.Reader(['en'])
-    result = ocr_reader.readtext(np.array(image))
-    ocr_texts = [line[1] for line in result]
-    return ocr_texts
+from transformers import pipeline
+from PIL import Image
+
+pipe = pipeline("object-detection", model="facebook/detr-resnet-50")
 img='BZ1A2269.jpg'
-print(perform_ocr(img))
+image=Image.open(img).convert("RGB")
+result=pipe(image)[0]
